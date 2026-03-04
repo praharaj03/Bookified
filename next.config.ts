1a71c05edd9dbe6277fcb09bb7a64362402780ab
ABHISEK PRAHARAJ <145381095+Abhisek250603@users.noreply.github.com>
2026-03-04 19:16:59 +0530

    build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: { remotePatterns: [
+    { protocol: "https", hostname: "covers.openlibrary.org" },
+    { protocol: "https", hostname: "SuOIeAaWuZ7VDxZK.public.blob.vercel-storage.com" }, // Vercel Blob Storage for user-uploaded images
+  ]}
 };
 
 export default nextConfig;
